3046b90e2dfc335bac53996debd6b1df47a012b0
Zhassulan <kenzhassulan@gmail.com>
2017-04-11 22:25:13 +0600

    123

---
+++ src/main/java/com/kenzh/projectA/topic/TopicController.java
@@ -0,0 +1,44 @@
+package com.kenzh.projectA.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kenzh on 4/1/17.
+ */
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics")
+    public void updateTopic(@RequestBody Topic topic) {
+        topicService.updateTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
+}
